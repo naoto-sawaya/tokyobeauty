9b6bbbc71ac31060089dcd7e1f6368265c2d8b5d
naoto.sawaya <naoto.sawaya@ssf-hd.co.jp>
2021-08-12 18:25:42 +0900

    お知らせ欄を更新

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+$metacsvloadid='top';
+include ($_SERVER['DOCUMENT_ROOT'].'/php/metacsv.php');
+?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/php/filename.php'); ?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#"><!--OGPの宣言-->
+<!--OGP-->
+<!--共通項目-->
+<meta property="og:title" content="女性の薄毛治療なら東京ビューティークリニックへ" /><!--ページのタイトル-->
+<meta property="og:type" content="article" /><!--ページのタイプ-->
+<meta property="og:url" content="https://www.tokyobeauty.jp/" /><!--ページのURL-->
+<meta property="og:image" content="https://www.tokyobeauty.jp/img/top/ogp02.png" /><!--サムネイル画像のURL-->
+<meta property="og:site_name" content="https://www.tokyobeauty.jp/" /><!--サイト名(TOPページのURL)-->
+<meta property="og:description" content="女性の薄毛でお悩みの方は、東京ビューティークリニックへ！薄毛治療・FAGA治療・女性育毛をお考えの方は、私たち専門医にご相談ください。電話・メール・LINEにて無料カウンセリング予約が可能。" /><!--ディスクリプション-->
+
+<!--Facebook用-->
+<meta property="fb:admins" content="1073741824" /><!--adminID-->
+<meta property="article:publisher" content="https://www.facebook.com/www.tokyobeauty.jp" /><!--FacebookページのURL-->
+
+<!--Twitter用-->
+<meta name="twitter:card" content="summary_large_image" /><!--Twitterカードの種類-->
+<meta name="twitter:site" content="@tbc_beauty" /><!--@Twitter-->
+<!--/OGP-->
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<title><?php echo $title;?></title>
+<meta name="description" content="<?php echo $description;?>" />
+<meta name="keywords" content="<?php echo $keywords;?>" />
+<link href="/common/css/reset.css?date=<?php echo_filedate($_SERVER['DOCUMENT_ROOT'].'/common/css/reset.css'); ?>" rel="stylesheet" type="text/css" >
+<link href="/common/css/base.css?date=<?php echo_filedate($_SERVER['DOCUMENT_ROOT'].'/common/css/base.css'); ?>" rel="stylesheet" type="text/css" >
+<link href="/css/top.css?date=<?php echo_filedate($_SERVER['DOCUMENT_ROOT'].'/css/top.css'); ?>" rel="stylesheet" type="text/css" >
+<link href="/css/share-button.css?date=<?php echo_filedate($_SERVER['DOCUMENT_ROOT'].'/css/share-button.css'); ?>" rel="stylesheet" type="text/css" />
+<link href="/common/css/jquery.bxslider.css?date=<?php echo_filedate($_SERVER['DOCUMENT_ROOT'].'/common/css/jquery.bxslider.css'); ?>" rel="stylesheet" type="text/css" >
+<link rel="shortcut icon" href="common/img/favicon.ico">
+<?php include"php/inHead.php"; ?>
+<script type="text/javascript" src="/common/js/jquery.bxslider.js"></script>
+</head>
+
+<body id="home">
+<!-- header --><?php include"php/header.php"; ?>
+<!--/header-->
+
+<!-- ▼mv -->
+<!--160523パタパタMV　ここから-->
+<div id="mv">
+	<script src="top_mv/top_mv.php" type="text/javascript"></script>
+	<div id="mvTop1" class="mv">
+		<ul id="mvMainview"></ul>
+			<p id="arrowU"><a href="/selfcheck/"><img src="top_mv/img/btn_selfcheck.png" width="218" height="155" alt="髪のお悩みセルフチェック" /></a></p>
+			<p id="arrowD"><a href="/clinic/"><img src="top_mv/img/btn_clinic.png" width="218" height="155" alt="全国で受けられる女性の発毛治療のクリニック一覧" /></a></p>
+
+	</div>
+</div>
+<!--160523パタパタMV　ここまで-->
+
+<div id="wrapper">
+	<!-- main -->
+	<div class="main clearfix">
+		<div class="clearfix section" id="syorei">
+			<h3 class="ttl">薄毛治療実績 ～症例紹介～</h3>
+			<p class="alignC mb20"><img src="/img/top/top_syorei.png" /></p>
+			<div class="syorei_slider">
+				<div class="slide"><a href="/photo/case76.php#wrapper"><img src="/img/top/syorei_01.png"></a><a href="/photo/case26.php#wrapper"><img src="/img/top/syorei_02.png"></a></div>
+
+				<div class="slide"><a href="/photo/case96.php#wrapper"><img src="/img/top/syorei_03.png"></a><a href="/photo/case3.php#wrapper"><img src="/img/top/syorei_04.png"></a></div>
+
+				<div class="slide"><a href="/photo/case126.php#wrapper"><img src="/img/top/syorei_05.png"></a><a href="/photo/case4173.php#wrapper"><img src="/img/top/syorei_06.png"></a></div>
+
+				<div class="slide"><a href="/photo/case52.php#wrapper"><img src="/img/top/syorei_07.png"></a><a href="/photo/case86.php#wrapper"><img src="/img/top/syorei_08.png"></a></div>
+
+				<div class="slide"><a href="/photo/case7.php#wrapper"><img src="/img/top/syorei_09.png"></a><a href="/photo/case36.php#wrapper"><img src="/img/top/syorei_10.png"></a></div>
+			</div>
+			<p class="left">当院の患者様症例、薄毛治療実績、発毛効果をご覧いただけます。<br />年代別の頭部写真で効果がひと目で分かります。</p>
+			<p class="btnText"><a href="/photo/">詳しくはこちら</a></p>
+		</div>
+
+		<div class="clearfix section" id="menu">
+			<div class="clearfix">
+				<h3 class="ttl">オリジナル薄毛・育毛治療</h3>
+				<p><img src="/img/top/microscope.png?date=<?php echo_filedate($_SERVER['DOCUMENT_ROOT'].'/img/top/microscope.png'); ?>" class="imgL" alt="マイクロスコープ" />当院は『女性のためだけに女性に特化した薄毛治療』を行うクリニックとして、女性のために最も効果的な治療法をご提案できるよう考えてきました。<br />
+例えば頭皮・頭髪の状態を確認するマイクロスコープ診断や当院オリジナルの発毛薬「 Rebirth Lady(リバース レディ)」、オリジナル発毛カクテルのDr‘sメソ(FAGAメソセラピー)などの治療方法を日々研究し、開発しております。</p>
+			<p class="btnText"><a href="/introduction/microscope/">【初回限定】マイクロスコープ無料診断はこちら</a></p>
+			</div>
+
+			<ul>
+				<li><a href="/menu/rebirth.php"><img src="/img/top/menu01.png" alt="女性用オリジナル発毛薬 Rebirth Lady(リバースレディ)" /></a></li>
+				<li class="last"><a href="/menu/meso.php"><img src="/img/top/menu02.png" alt=" 頭髪外来 Dr'sメソ(FAGAメソセラピー)" /></a></li>
+				<li><a href="/menu/syokumo.php"><img src="/img/top/menu04.png" alt="Hair Moving 植毛法" /></a></li>
+				<li><a href="/menu/smartgraft.php"><img src="/img/top/menu08.png" alt="スマートグラフト自毛植毛" /></a></li>
+				<li class="last"><a href="/menu/stem_cell.php"><img src="/img/top/menu03.png" alt=" FAGA幹細胞再生治療" /></a></li>
+				<li><a href="/menu/hairbeam.php"><img src="/img/top/menu09.png" alt="低出力レーザー治療ヘアビーム" /></a></li>
+				<li><a href="/menu/pantogar.php"><img src="/img/top/menu05.png" alt="頭髪外来 パントガール" /></a></li>
+				<li class="last"><a href="/menu/balumo.php"><img src="/img/top/menu06.png" alt="医療用（育毛）シャンプー「Dr.BALUMO」" /></a></li>
+				<li><a href="/menu/glashvista.php"><img src="/img/top/menu07.png" alt="まつ毛の育毛剤 グラッシュビスタ" /></a></li>
+
+			</ul>
+		</div>
+
+		<div class="section clearfix" id="docs">
+			<h3 class="ttl">全国のクリニックのご紹介</h3>
+			<p>女性の頭髪治療に実績のある東京ビューティークリニックの直営院をご紹介します。<span class="holiday">土日祝も開院</span></p>
+			<ul class="doctor">
+				<li><img src="/img/top/dr01.png" /></li>
+				<li><img src="/img/top/dr02.png" /></li>
+				<!--li><img src="/img/top/dr03.png" /></li-->
+				<li><img src="/img/top/dr04.png" /></li>
+			</ul>
+			<ul class="cl_list clearfix" >
+				<li><a href="/clinic/ginza.php"><span>銀座院</span></a></li>
+				<li><a href="/clinic/shibuya.php"><span>渋谷院</span></a></li>
+				<li class="long"><a href="/clinic/shinjuku.php"><span>新宿アイランド<br />タワー院</span></a></li>
+				<li><a href="/clinic/yokohama.php"><span>横浜院</span></a></li>
+			</ul>
+			<ul class="cl_list clearfix" >
+				<li><a href="/clinic/omiya.php"><span>大宮院</span></a></li>
+				<li><a href="/clinic/sapporo.php"><span>札幌院</span></a></li>
+				<li><a href="/clinic/nagoya.php"><span>名古屋院</span></a></li>
+				<li><a href="/clinic/umeda.php"><span>大阪 梅田院</span></a></li>
+			</ul>
+			<ul class="cl_list clearfix" >
+				<li><a href="/clinic/namba.php"><!--img src="/img/top/open_cam.png?date=<?php echo_filedate($_SERVER['DOCUMENT_ROOT'].'/img/top/open_cam.png'); ?>" class="new_cam"/--><span>大阪 難波院</span></a></li>
+				<li><a href="/clinic/fukuoka.php"><span>福岡院</span></a></li>
+			</ul>
+			<!--div class="touhatsu clearfix">
+				<p class="right"><a href="/clinic/sapporo.php"><img src="/img/top/shindan_hokkaidou.png?date=<?php echo_filedate($_SERVER['DOCUMENT_ROOT'].'/img/top/shindan_hokkaidou.png'); ?>" /></a></p>
+				<p class="right"><a href="/clinic/shibuya.php"><img src="/img/top/shindan_kantou.png" /></a></p>
+			</div-->
+		</div>
+
+		<div class="section clearfix" id="detail">
+			<h3 class="ttl">薄毛・抜け毛にお悩みの方へ</h3>
+			<p class="mb20">男性だけでなく20代以降の女性にも深刻な「薄毛」の悩みが増えています。年齢のせいだから仕方ないとあきらめる前に、女性薄毛の治療病院へ一度お問合せください。</p>
+			<dl>
+				<a href="/introduction/reason/">
+				<dt>当院が選ばれている5つの理由</dt>
+				<dd><img src="/img/top/clinic_treatment01.png" class="imgL"/>女性の発毛外来「東京ビューティークリニック」が多くの患者様に選ばれている理由をご紹介します。</dd></a>
+			</dl>
+			<dl>
+				<a href="/survey/">
+				<dt>女性の薄毛の意識調査</dt>
+				<dd><img src="/img/top/clinic_treatment02.png" class="imgL"/>薄毛に対するコンプレックスは20代～40代の多くの女性が感じていました。</dd></a>
+			</dl>
+			<dl>
+				<a href="/movie/">
+				<dt>発毛体験談・アンケート紹介</dt>
+				<dd><img src="/img/top/clinic_treatment03.png" class="imgL"/>当院の薄毛治療を体験された方々からの声を紹介します。</dd></a>
+			</dl>
+			<dl>
+				<a href="/introduction/qa/">
+				<dt>女性の薄毛に関するQ&amp;A</dt>
+				<dd><img src="/img/top/clinic_treatment04.png" class="imgL"/>初めて治療される方のために<br />薄毛治療に関する疑問点にお答えします。</dd></a>
+			</dl>
+		</div>
+
+		<div class="information">
+			<h2><img src="img/top/info_ttl.gif" alt="information" /></h2>
+			<dl class="clearfix">
+              <dt>2021.08.12</dt>
+              <dd>Gitのテスト開始しました。</dd>
+				<dt>2018.02.28</dt>
+              <dd><a href="/campaign/#camp03">「髪のお悩み卒業キャンペーン」を開始しました！</a></dd>
+              <dt>2018.02.23</dt>
+              <dd><a href="/media/archive21.php">『VoCE』4月号に掲載されました。</a></dd>
+              <dt>2018.02.21</dt>
+              <dd><a href="/media/archive20.php">『西日本新聞』2月10日朝刊に掲載されました。</a></dd>
+              <dt>2018.01.16</dt>
+              <dd><a href="https://www.agaskin.net/se/clinic/" target="_blank">1月末 提携院のAGAスキンクリニック 大阪堺東院が開院します。</a></dd>
+              <dt>2018.01.11</dt>
+              <dd><a href="/media/archive19.php">2018.1.10放送 FRESH!『宝塚OGチャンネル』で紹介されました。</a></dd>
+              <dt>2017.12.06</dt>
+              <dd><a href="/media/archive18.php">『女性自身』12月19日号に掲載されました。</a></dd>
+              <!--<dt>2017.12.01</dt>
+              <dd><span class="f_red02">【年末年始開院日のご案内】<br />
+年末は12月30日（土）まで、年始は1月4日（木）から開院しております。<br />
+お問い合わせ・無料カウンセリングご予約は年中無休で受付けております。</span><br />
+※提携院（AGAスキンクリニック・東京美容外科）は異なる場合がございますのでお問い合わせ下さい。<br />
+※電話受付時間が一部変更となります。<br />＜12/31(日) 9:00～19:00、1/1(月・祝) 10:00～19:00、1/2(火・祝) 9:00～20:00、1/3(水・祝) 9:00～21:00＞<br />お急ぎの場合は、24時間対応のLINEでお問合わせください。</dd>-->
+              <!--<dt>2017.10.26</dt>
+              <dd><a href="/activities/archive02.php">2017.10.23（月）トークイベント『女性の薄毛治療最先端～ネイル・エステよりも見た目年齢に関わる髪の話～』を開催しました。</a></dd>-->
+             <!-- <dt>2017.10.25</dt>
+              <dd><a href="/line/">初めての方も・通院中の方も！LINE予約が24時間対応になりました。</a></dd>-->
+              <!--<dt>2017.10.19</dt>
+              <dd><a href="/menu/hairbeam.php">低出力レーザー「ヘアビーム」の販売を開始しました。</a></dd>-->
+			</dl>
+		</div>
+
+		<div class="news clearfix" id="media">
+			<p class="alignC"><a href="/media/"><img src="/img/top/media_top.png" /></a></p>
+			<p class="mt10"><a href="/media/archive21.php">『VoCE』4月号に掲載されました</a></p>
+			<p class="btnText"><a href="/media/archive21.php">詳しくはこちら</a></p>
+		</div>
+
+
+		<!-- cb_info -->
+		<?php include"php/btn_info.php"; ?>
+		<!-- /cb_info -->
+
+		<div id="top_about_site" class="clearfix">
+			<div id="about_site">
+<p>ストレス社会の現代、“薄毛といえば男性の悩み”だけではありません。薄毛や抜け毛、自然脱毛などの悩みを持つ女性は日本で10人に1人とされています。男性だけでなく20代以降の女性にも深刻な「薄毛」の悩みが増えています。<br />
+女性の薄毛は女性型脱毛症（FAGA）といって、髪の毛の量が全体的にボリュームダウンしてきます。<br />
+男性型脱毛症（AGA）のように前頭部や頭頂部など特定の部分から集中的に髪が抜けて薄くなるわけではないため、薄くなったと気付くのが遅くなりがちです。<br />
+東京ビューティークリニックは麻生泰開発者兼統括診療部長自身が薄毛に悩んでいた経験から、薄毛に悩む男性のために発毛薬『Rebirth』を開発しました。その『Rebirth』の実績を元に、女性の薄毛のメカニズムを研究、解析して、開発したのが女性専用の『Rebirth Lady』（リバースレディ）です。<br />
+当院は薄毛で悩む全ての女性に、原因を追究した安心･安全な女性薄毛治療を行っています。</p>
+			</div>
+		</div>
+
+	</div>
+	<!--/main-->
+
+	<!-- sidemenu -->
+	<div id="sidemenu">
+		<!--rnav_share-->
+		<?php include"php/rnav_share.php"; ?>
+		<!--/rnav_share-->
+		<!--rnav-->
+		<?php include"php/rnav.php"; ?>
+		<!--/rnav-->
+	</div>
+	<!-- / sidemenu -->
+</div>
+<!--/wrapper-->
+
+<!-- footer  -->
+<?php include"php/footer.php"; ?>
+<!--/footer-->
+
+<script type="text/javascript" src="/common/js/share-button.js"></script>
+<script type="text/javascript" src="/common/js/smoothscroll.js"></script>
+<script type="text/javascript" src="/common/js/_a_cast.js"></script>
+<script type="text/javascript" src="/common/js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="/common/js/jquery.fitvids.js?date=<?php echo_filedate($_SERVER['DOCUMENT_ROOT'].'/common/js/jquery.fitvids.js'); ?>"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+  $('.syorei_slider').bxSlider({
+    slideWidth: 657,
+    minSlides: 1,
+    maxSlides: 1,
+    slideMargin: 10
+  });
+});
+</script>
+
+<!--tag_RM-->
+<?php include"php/tag_RM.php"; ?>
+<!--/tag_RM-->
+
+
+</body>
+</html>
